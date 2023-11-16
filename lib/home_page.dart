468706a4cf745ba1669f9f27eab26c7f7c74f8dd
unknown <kowshikdas001@gmail.com>
2023-11-17 03:03:59 +0600

    create homepage

---
+++ lib/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyHomePageextends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
